6fa8e64b48a9bf96ab4609f60fea01997e000125
HoonHee Jeong <hhjeong@me.com>
2021-04-03 19:32:40 +0900

    fail on unknown properties

--- src/test/java/me/hhjeong/restapiwithspring/events/EventControllerTests.java
+++ src/test/java/me/hhjeong/restapiwithspring/events/EventControllerTests.java
@@ -4,12 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -34,7 +31,39 @@ public class EventControllerTests {
 	@Test
 	@DisplayName("이벤트 생성")
 	public void createEvent() throws Exception {
+		EventDto event = EventDto.builder()
+				.name("Spring")
+				.description("REST API Development with Spring")
+				.beginEnrollmentDateTime(LocalDateTime.of(2021,4, 3, 16, 37))
+				.closeEnrollmentDateTime(LocalDateTime.of(2021, 4, 4, 16, 37))
+				.beginEventDateTime(LocalDateTime.of(2021, 4, 14, 16, 37))
+				.endEventDateTime(LocalDateTime.of(2021, 4, 15, 16, 37))
+				.basePrice(100)
+				.maxPrice(200)
+				.limitOfEnrollment(100)
+				.location("서울상공회의소 개방형 클라우드 플랫폼 센터")
+				.build();
+
+		mockMvc.perform(post("/api/events/")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaTypes.HAL_JSON)
+				.content(objectMapper.writeValueAsString(event)))
+				.andDo(print())
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("id").exists())
+				.andExpect(header().exists(HttpHeaders.LOCATION))
+				.andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+				.andExpect(jsonPath("id").value(Matchers.not(100)))
+				.andExpect(jsonPath("free").value(Matchers.not(true)))
+				.andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
+		;
+	}
+
+	@Test
+	@DisplayName("이벤트 생성(입력값 이외에 에러 발생)")
+	public void createEvent_BadRequest() throws Exception {
 		Event event = Event.builder()
+				.id(100)
 				.name("Spring")
 				.description("REST API Development with Spring")
 				.beginEnrollmentDateTime(LocalDateTime.of(2021,4, 3, 16, 37))
@@ -55,13 +84,7 @@ public class EventControllerTests {
 				.accept(MediaTypes.HAL_JSON)
 				.content(objectMapper.writeValueAsString(event)))
 				.andDo(print())
-				.andExpect(status().isCreated())
-				.andExpect(jsonPath("id").exists())
-				.andExpect(header().exists(HttpHeaders.LOCATION))
-				.andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-				.andExpect(jsonPath("id").value(Matchers.not(100)))
-				.andExpect(jsonPath("free").value(Matchers.not(true)))
-				.andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT))
+				.andExpect(status().isBadRequest())
 		;
 	}
 
