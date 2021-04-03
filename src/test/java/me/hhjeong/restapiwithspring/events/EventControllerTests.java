4cc8c1391b427cba74bee5176b6134fec1d54e19
HoonHee Jeong <hhjeong@me.com>
2021-04-03 16:24:38 +0900

    add create events test

---
+++ src/test/java/me/hhjeong/restapiwithspring/events/EventControllerTests.java
@@ -0,0 +1,30 @@
+package me.hhjeong.restapiwithspring.events;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class EventControllerTests {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@Test
+	@DisplayName("이벤트 생성")
+	public void createEvent() throws Exception {
+		mockMvc.perform(post("/api/events/")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaTypes.HAL_JSON)
+				)
+				.andExpect(status().isCreated());
+	}
+
+}
